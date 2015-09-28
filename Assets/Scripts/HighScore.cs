fcf5b07269af322dede0aff92e7c63d9be235815
Dhaval <Dhaval@nbp-145-229.nbp.ruw.rutgers.edu>
2015-09-28 18:32:08 -0400

    init

---
+++ Assets/Scripts/HighScore.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class HighScore : MonoBehaviour {
+
+	static public int score = 1000;
+
+	public Text hs;
+
+	void Start() {
+		hs = this.GetComponent<Text> ();
+	}
+
+	void Awake () {
+		if (PlayerPrefs.HasKey("ApplePickerHighScore")) {
+			score = PlayerPrefs.GetInt("ApplePickerHighScore");
+			PlayerPrefs.SetInt("ApplePickerHighScore", score);
+		}
+	}
+
+	void Update () {
+		hs.text = "Highscore : " + score;
+		if (score > PlayerPrefs.GetInt("ApplePickerHighScore")) {
+			PlayerPrefs.SetInt("ApplePickerHighScore", score);
+		}
+	}
+}
